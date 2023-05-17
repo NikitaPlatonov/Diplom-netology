d04ea644e31c32b1dc121ded28d9eb8483cb8451
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-17 10:04:27 +0300

    настроил toString -> PageEntry в формате json

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,20 @@
+import com.google.gson.Gson;
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    private static Gson gson = new Gson();
+
+    public PageEntry(String pdfName, int page, int count){
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return gson.toJson(this);
+    }
 }
