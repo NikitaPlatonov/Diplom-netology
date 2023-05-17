adb285af81746f5359550a7e81ffe7a58991e4b4
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-17 15:51:08 +0300

    сделал ответ клиенту на то, если такого слово в pdf-файлах нет

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,22 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
         try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            System.out.println("Сервер запущен");
             while (true) {
                 try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+                    System.out.println("Подключился клиент с таким портом " + socket.getPort());
                     String expectedWord = in.readLine();
-                    out.println(engine.search(expectedWord));
+                    if(engine.search(expectedWord).equals(Collections.emptyList())){
+                        out.println("Совпадений нет");
+                    } else {
+                        out.println(engine.search(expectedWord));
+                    }
                 }
             }
         } catch (IOException e) {
