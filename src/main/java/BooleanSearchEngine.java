5caf6667b35073049a50dcb43caa034f88d5a869
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-17 08:38:12 +0300

    настроил конструктор BooleanSearchEngine

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,13 +1,39 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private StringBuilder text = new StringBuilder();
+    private String[] words;
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        File directory = new File("pdfs");
+        if(directory.exists() && directory.isDirectory()){
+            File[] pdfs = directory.listFiles();
+            if(pdfs != null) {
+                for (File pdfFile : pdfs) {
+                    var doc = new PdfDocument(new PdfReader(pdfFile));
+                    for(int i = 0; i < doc.getNumberOfPages();i++){
+                        var textFromPage = PdfTextExtractor.getTextFromPage(doc.getPage(i + 1));
+                        text.append(textFromPage);
+                    }
+                }
+                words = text.toString().split("\\P{L}+");
+            }
+        }
+        Map<String, Integer> freqs = new HashMap<>();
+        for (var word : words) {
+            if (word.isEmpty()) {
+                continue;
+            }
+            word = word.toLowerCase();
+            freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+        }
     }
 
     @Override
