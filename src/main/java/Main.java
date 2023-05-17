b0fc7b23cb599a053a665bd001be56eab3da84d2
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-17 15:27:01 +0300

    добавил сервер -> Main -> main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
-
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(((Socket) socket).getInputStream())); PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+                    String expectedWord = in.readLine();
+                    out.println(engine.search(expectedWord));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
