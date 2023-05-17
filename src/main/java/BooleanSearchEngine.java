23bf014bfbb86101740afa7aa671e683baca81c7
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-17 09:50:42 +0300

    исправил конструктор BooleanSearchEngine

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -8,31 +8,41 @@ import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    private StringBuilder text = new StringBuilder();
-    private String[] words;
-
+    private Map<String , List<PageEntry>> pageEntry = new HashMap<>();
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        File directory = new File("pdfs");
-        if(directory.exists() && directory.isDirectory()){
-            File[] pdfs = directory.listFiles();
+        if(pdfsDir.exists() && pdfsDir.isDirectory()){
+            File[] pdfs = pdfsDir.listFiles();
             if(pdfs != null) {
                 for (File pdfFile : pdfs) {
                     var doc = new PdfDocument(new PdfReader(pdfFile));
                     for(int i = 0; i < doc.getNumberOfPages();i++){
-                        var textFromPage = PdfTextExtractor.getTextFromPage(doc.getPage(i + 1));
-                        text.append(textFromPage);
+                        PdfPage pdfPage = doc.getPage(i + 1);
+                        String[] words = pdfPage.toString().split("\\p{L}+");
+
+                        Map<String, Integer> freqs = new HashMap<>();
+                        for (var word : words) {
+                            if (word.isEmpty()) {
+                                continue;
+                            }
+                            word = word.toLowerCase();
+                            freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                        }
+
+                        for(String word : words){
+                            word = word.toLowerCase();
+                            PageEntry query = new PageEntry(pdfFile.getName(), i + 1, freqs.get(word));
+                            List<PageEntry> pageEntries;
+                            if(pageEntry.containsKey(word)){
+                                pageEntries = pageEntry.get(word);
+                            } else{
+                                pageEntries = new ArrayList<>();
+                            }
+                            pageEntries.add(query);
+                            pageEntry.put(word, pageEntries);
+                        }
                     }
                 }
-                words = text.toString().split("\\P{L}+");
             }
-        }
-        Map<String, Integer> freqs = new HashMap<>();
-        for (var word : words) {
-            if (word.isEmpty()) {
-                continue;
-            }
-            word = word.toLowerCase();
-            freqs.put(word, freqs.getOrDefault(word, 0) + 1);
         }
     }
 
